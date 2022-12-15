0352d0e0a8751266ea0555173ff367ba55b2eded
Ethan <ethan.stuchlik@yahoo.com>
2022-12-16 00:54:16 +1100

    Sidetracked - CSS Styling and Shop Items Added

---
+++ Modules/index.js
@@ -0,0 +1,46 @@
+addShopItem = (shopItem) => {  // addshopItem makes an object and renders shopItems on Website
+  const shopItemHTML =
+    '<div class="shopItemBoxOuter">\n' + // Shop item container
+    '    <div class="shopItemBoxInner">\n' + // shopItem container for shopItem Text
+    '        <div class="shopItemImgWrapper">\n' +
+    '            <img class="shopItemImg" alt=' + shopItem.shopItemName + ' src=' + shopItem.shopItemImage + ' >\n' +
+    '        </div>\n' +
+    '        <div class="shopItemBoxInfo">\n' +
+    '            <p style="margin: 0px; font-weight: bold;">' + shopItem.shopItemName + '</p>\n' +
+    '            <p><b>Description:</b> ' + shopItem.shopItemDescription + '</p>\n' +
+    '            <p><b>Price:</b> ' + shopItem.shopItemPrice + '</p>\n' +
+    '            <button class="buy">Buy ' + shopItem.shopItemName + '</button>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</div>\n';
+  const shopItemContainer = document.getElementById("app");
+  shopItemContainer.innerHTML += shopItemHTML;
+};
+
+addShopItem({
+    'shopItemName':'Cooler Master Hyper212X',
+    'shopItemImage':'../Images/Cooler-Master-Hyper212X.jpg',
+    'shopItemDescription':'Tested, Works, runs smoothly and quietly',
+    'shopItemPrice':'AU$40'
+});
+
+addShopItem({
+    'shopItemName':'AMD Ryzen 5 2600',
+    'shopItemImage':'../Images/AMD-Ryzen-5-2600.jpg',
+    'shopItemDescription':'Tested, Works and minor scratches to IHS',
+    'shopItemPrice':'AU$120'
+});
+
+addShopItem({
+    'shopItemName':'EVGA GTX 1070',
+    'shopItemImage':'../Images/EVGA-GTX-1070.jpg',
+    'shopItemDescription':'Tested, Works',
+    'shopItemPrice':'AU$225'
+});
+
+addShopItem({
+    'shopItemName':'Kingston DDR3 4x8GB 1600MHZ',
+    'shopItemImage':'../Images/Kingston-4x8gb-ddr3-1600Mhz.jpg',
+    'shopItemDescription':'Tested, Works',
+    'shopItemPrice':'AU$130'
+});
